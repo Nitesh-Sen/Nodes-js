c3759a091bd04aea17881ca562538af89bc4eb23
AbhishekAbhisharma <146970101+AbhishekAbhisharma@users.noreply.github.com>
2024-10-20 11:58:12 +0530

    Update cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,6 +2,8 @@
 
 # Remove all files from the deployment directory
 echo "Cleaning up existing files in /home/ubuntu/Nodes"
+rm -rf /hone/ubuntu/*
 rm -rf /home/ubuntu/Nodes/*
+
 
 echo "Cleanup complete."
