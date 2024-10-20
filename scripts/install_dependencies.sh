dec61b9d0225d94a0fbc04f4e56972fdc32c2963
AbhishekAbhisharma <146970101+AbhishekAbhisharma@users.noreply.github.com>
2024-10-20 10:05:03 +0530

    Create install_dependencies.sh

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "Installing system dependencies"
+sudo apt update && sudo apt upgrade -y
+sudo apt install git -y
+sudo apt-get install ruby -y
+
+echo "Installing CodeDeploy agent"
+wget https://aws-codedeploy-ap-south-1.s3.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+sudo service codedeploy-agent start
+sudo service codedeploy-agent status
+
+echo "Installing Node.js"
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs
+node -v
+npm -v
+
+echo "Cloning the GitHub repository"
+cd /home/ubuntu/
+git clone https://github.com/AbhishekAbhisharma/Nodes.git
+cd Nodes/
+echo "Installing Node.js dependencies"
+npm install
