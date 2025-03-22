4c41e215ca92c20dcaf8dc698de83a310ae3e29d
AbhishekAbhisharma <146970101+AbhishekAbhisharma@users.noreply.github.com>
2025-03-23 00:15:21 +0530

    Update start_pm2.sh

--- scripts/start_pm2.sh
+++ scripts/start_pm2.sh
@@ -4,5 +4,6 @@ cd /home/ubuntu/Nodes
 pm install  # Ensure dependencies are installed
 pm run build  # Build the application if needed
 pm2 start index.js --name nodejs-app --watch
+pm2 restart nodejs-app
 pm2 save
 pm2 startup systemd
