1b6b181367cd14b503e99bc2816b6a29671df949
AbhishekAbhisharma <146970101+AbhishekAbhisharma@users.noreply.github.com>
2025-03-22 23:08:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! This is my Node.js app!');
+  res.send('Hello, Abhishek Sharma! This is your Node.js applications!');
 });
 
 app.listen(port, () => {
