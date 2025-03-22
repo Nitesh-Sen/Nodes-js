17ebf175d7ada5f0dd5d7c9631a1858d4f14611b
AbhishekAbhisharma <146970101+AbhishekAbhisharma@users.noreply.github.com>
2025-03-22 23:47:48 +0530

    Update start_pm2.sh
    update file

--- scripts/start_pm2.sh
+++ scripts/start_pm2.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 echo "Starting application with PM2"
-pm2 start /home/ubuntu/Nodes/index.js --name nodejs-app
+cd /home/ubuntu/Nodes
+pm install  # Ensure dependencies are installed
+pm run build  # Build the application if needed
+pm2 start index.js --name nodejs-app --watch
 pm2 save
+pm2 startup systemd
