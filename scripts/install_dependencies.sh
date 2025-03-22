08fab1747a62ea3b5a6c3f95b2dc6e985ec9c47d
AbhishekAbhisharma <146970101+AbhishekAbhisharma@users.noreply.github.com>
2025-03-22 23:47:09 +0530

    Update install_dependencies.sh
    update file

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
+
 echo "Installing system dependencies"
-sudo -i
 sudo apt update && sudo apt upgrade -y
-sudo apt install git -y
-sudo apt-get install ruby -y
+sudo apt install -y git ruby
 
+# Install AWS CodeDeploy agent
 echo "Installing CodeDeploy agent"
-wget https://aws-codedeploy-ap-south-1.s3.amazonaws.com/latest/install
-chmod +x ./install
-sudo ./install auto
-sudo service codedeploy-agent start
-sudo service codedeploy-agent status
+wget https://aws-codedeploy-ap-south-1.s3.amazonaws.com/latest/install -O install_codedeploy
+chmod +x install_codedeploy
+sudo ./install_codedeploy auto
+sudo systemctl enable codedeploy-agent
+sudo systemctl start codedeploy-agent
+sudo systemctl status codedeploy-agent --no-pager
 
+# Install Node.js & PM2
 echo "Installing Node.js"
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install -y nodejs
 node -v
 npm -v
 
-# Install PM2 globally
 echo "Installing PM2 globally"
 sudo npm install pm2@latest -g
 
-# Check if the 'Nodes' directory exists and remove it if it does
+# Ensure 'Nodes' directory is removed before cloning
 if [ -d "/home/ubuntu/Nodes" ]; then
   echo "Removing existing Nodes directory"
   rm -rf /home/ubuntu/Nodes
@@ -32,7 +33,6 @@ echo "Cloning the GitHub repository"
 cd /home/ubuntu/
 git clone https://github.com/AbhishekAbhisharma/Nodes.git
 cd Nodes/
+
 echo "Installing Node.js dependencies"
 npm install
-
-
